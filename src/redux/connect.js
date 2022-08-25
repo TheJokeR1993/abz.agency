7bb1eb5103406ffe8c96add358c71c6fdccdeed7
TheJokeR1993 <thejokermafiaclub@gmail.com>
2022-08-25 23:40:24 +0300

    first api change

--- src/redux/connect.js
+++ src/redux/connect.js
@@ -3,7 +3,7 @@ import { firstStateUsers } from "./card/cardRedux";
 import {  getPosition} from "./form/formRedux";
 
 export const connectFirstCard = () => dispatch =>{
-    api.users()
+    api.users(1)
     .then(i=>dispatch(firstStateUsers(i)))
     api.positions()
     .then((i) =>dispatch(getPosition(i.positions)))
