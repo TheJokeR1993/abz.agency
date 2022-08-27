31017e06a0df4ced7d11af89853909cc49bd212c
TheJokeR1993 <thejokermafiaclub@gmail.com>
2022-08-27 13:02:49 +0300

    webp change jpg

--- src/Component/heading/Heading.js
+++ src/Component/heading/Heading.js
@@ -1,6 +1,7 @@
 import   '../../index.scss'
 function Heading() {
-    return <div className='container heading'>
+    
+    return <div className='container heading '>
         <div className='heading_block'>
             <h1>Test assignment for front-end developer</h1>
             <p>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
